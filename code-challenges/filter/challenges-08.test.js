67e87b56accae7a0e01e0bd9c0c2137fd3421611
RAVEN DELANEY <ravend17@gmail.com>
2019-10-24 14:51:53 -0700

    stretch goal in class

--- code-challenges/filter/challenges-08.test.js
+++ code-challenges/filter/challenges-08.test.js
@@ -163,7 +163,10 @@ For example: evenOddNumericValues(['Gregor', 2, 4, 1]) returns ['even', 'even', 
 ------------------------------------------------------------------------------------------------ */
 
 const evenOddNumericValues = (arr) => {
-  // Solution code here...
+  let numArr = arr.filter(value => typeof value !== 'string');
+  return numArr.map(value =>{
+    return value % 2 === 0 ? 'even' : 'odd';
+  });
 };
 
 /* ------------------------------------------------------------------------------------------------
@@ -262,7 +265,7 @@ xdescribe('Testing challenge 6', () => {
   });
 });
 
-xdescribe('Testing challenge 7', () => {
+describe('Testing challenge 7', () => {
   test('It should remove non-integers and return "even" or "odd', () => {
     expect(evenOddNumericValues(['Gregor', 2, 4, 1])).toStrictEqual(['even', 'even', 'odd']);
     expect(evenOddNumericValues(['Gregor', 2, 4, 1]).length).toStrictEqual(3);
