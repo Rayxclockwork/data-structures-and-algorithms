cd16a23b3d607fc2ee865ed9eaafce62fbb5c0ac
RAVEN DELANEY <ravend17@gmail.com>
2019-10-23 20:33:11 -0700

    working on test 4

--- code-challenges/filter/challenges-08.test.js
+++ code-challenges/filter/challenges-08.test.js
@@ -84,9 +84,7 @@ const snorlaxData = {
 };
 
 const getBaseStatGreaterThan = (arr, minBaseStat) => {
-  let stat = arr.filter(baseStat => {
-    baseStat.baseStat > minBaseStat});
-  return stat;
+  return arr.filter()
 };
 
 /* ------------------------------------------------------------------------------------------------
@@ -226,7 +224,7 @@ describe('Testing challenge 3', () => {
   });
 });
 
-xdescribe('Testing challenge 4', () => {
+describe('Testing challenge 4', () => {
   test('It should return an array containing the stats that are greater than the input', () => {
     expect(getBaseStatGreaterThan(snorlaxData.stats, 75)).toStrictEqual([ { stat: { url: 'https://pokeapi.co/api/v2/stat/5/', name: 'special-defense' }, effort: 2, baseStat: 110 } ]);
     expect(getBaseStatGreaterThan(snorlaxData.stats, 75).length).toStrictEqual(1);
